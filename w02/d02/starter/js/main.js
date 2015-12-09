2f3ed1b4f1baf1eada5e3effa80a6fa57a5289c9
kati pelletier <katherine.pelletier@gmail.com>
2015-12-09 09:41:50 -0600

    w02d02 HW

--- w02/d02/starter/js/main.js
+++ w02/d02/starter/js/main.js
@@ -2,3 +2,30 @@
 // One way to get input is to use the prompt() command
 // You can have your adventure play out using alerts or in the console using console.log().
 
+
+var firstName = prompt("What is your first name?");
+  alert("Hello, " + firstName);
+
+alert("There is a possible chemical spill and we need an ICS commander.")
+
+var ICS = prompt("Have you led an ICS incident before? yes or no");
+  switch (ICS) {
+  case "yes":
+    alert("Great! We need you.");
+    break;
+  case "no":
+    alert("It may be a long night.");
+    break;
+}
+
+var need = prompt("How many gallons have spilled, between 1 - 100?");
+ if (need <= 20) {
+ alert(need + " is not too much. Let's get the kitty litter.");
+} else
+    if (need <= 60) {
+ alert(need + " is enough for HazMat team.");
+} else
+  if (need <= 100) {
+ alert("We will have to call local authorities.")
+}
+
