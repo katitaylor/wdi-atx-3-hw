9dd47958e5f049cc3e7947d035e9c4aa64440a96
kati pelletier <katherine.pelletier@gmail.com>
2015-12-11 09:13:27 -0600

    worked through logic and began functions, but not functioning

--- w02/d04/starter/js/atm.js
+++ w02/d04/starter/js/atm.js
@@ -1,40 +1,85 @@
-//Begin with the document ready function
 
-	//Begin work on the Checking Account
+$("document").ready(function() {
 
-		//Checking account deposit function
+// #amountChecking = input (text)
+// var a = checkingBalance
+// var b = depositChecking
+// var c = withdrawChecking
 
-			//Get value from the amountChecking input field
+function addChecking {
+
+
+
+*//Begin with the document ready function
+
+  //Begin work on the Checking Account
+
+    //Checking account deposit function
+
+
+
+      //Get value from the amountChecking input field
+
+$("#amountChecking, #checkingBalance").on("click", function)(event) {
+        alert($(this).text());
+        console.log(function);
+        console.log(this);
+      });
+
+
 
 			//On click of the depositChecking button
+$(#depositChecking).on("click", addMoney());
 
 				//Take that value and add it to the existing value in the checkingBalance div
+function addItUp {
+    sum = #checkingBalance + ($(this))
+}
 
-		//Checking account withdrawl funtion 
+		//Checking account withdrawl funtion
 
 			//Get value from the amountChecking input field
-			
+$("#amountChecking").on("click", function)(event) {
+        alert($(this).text());
+      });
+
+});
 			//On click of the withdrawChecking button
+$(#withdrawChecking).on("click", subtractMoney());
 
 				// If that value is greater than the value in the account ignore that action
-				// In other words if this would put the account into a negative balance do not allow it
+if (true) {
+c > a;
+(result = false);
+};
+
+else {
+  (sum = c-a);
+}
+});
+        // In other words if this would put the account into a negative balance do not allow it
 
 				//Else subtract that value from the current amount in the checking account
 
 	//Begin work on the Savings account
 
-		//Savings account deposit function
+// #amountSavings = input (text)
+// var a = savingsBalance
+// var b = savingsChecking
+// var c = withdrawS avings
 
+		//Savings account deposit function
+function addSavings {
 		 		//Get value from the amountSavings input field
 
 		 		//On click of the depositSavings button
 
 		 			//Take that value and add it to the existing value in the savingsBalance div
 
-		//Savings account withdraw funtion 
+		//Savings account withdraw funtion
 
 		 		//Get value from the amountSavings input field
-				
+
 		 		//On click of the withdrawl button
 
 		 			//If that value is greater than the value in the account ignore that action
