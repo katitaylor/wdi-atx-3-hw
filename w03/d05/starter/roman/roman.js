397622e9d627a0db2e9cb8545ceb33d47373cc3a
kati pelletier <katherine.pelletier@gmail.com>
2015-12-27 11:12:51 -0600

    romanize decimals

--- w03/d05/starter/roman/roman.js
+++ w03/d05/starter/roman/roman.js
@@ -1,14 +1,21 @@
-function romanize(number){
-  var matches = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X"
+function romanize(number) {
+  var matches = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+  for ( i in matches ) {
+    while ( number >= matches[i] ) {
+      roman += i;
+      number -= matches[i];
+    }
   }
+  return roman;
 }
+console.log(romanize(9));
+
+//##Roman Numeral Converter
+
+// -Create a function *romanize* that takes a number as an input and converts that number into a roman numeral.
+// ``` romanize(946) => CMXLIII;
+//   romanize(55) => LV;
+//   romanize(46) => XLVI```
+
+// -[Here is a helpful chart](http://literacy.kent.edu/Minigrants/Cinci/romanchart.htm)
+// - **Dont forget that M = 1000 and D = 500**
