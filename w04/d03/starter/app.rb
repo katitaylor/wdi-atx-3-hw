329c396f95a93fc8cf0d87f9db972bd3ae1cc6b8
kati pelletier <katherine.pelletier@gmail.com>
2016-01-06 21:32:49 -0600

    install and layout

--- w04/d03/starter/app.rb
+++ w04/d03/starter/app.rb
@@ -3,15 +3,71 @@ require 'sinatra/reloader'
 
 # Coin toss
 get '/coin_toss' do
-	
+
 end
 
 # Dice roll
 get '/dice_roll' do
-	
+
 end
 
-# Magic 8 ball 
+# Magic 8 ball
 get '/magic8ball/:question' do
+
+def answers
+  [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ]
+end
+
+def random_answer
+  answers[Random.rand(answers.size)]
+end
+
+def shake_ball
+  Array.new(1 + Random.rand(5)).map{ |e| "[shake]" }.join(" ")
+end
+
+def wait_for_user
+  question = gets.chomp
+  exit(0) if question == "q"
+end
+
+def prompt
+  "Ask the Magic 8-Ball a question! (q to quit)"
+end
+
+if __FILE__ == $0
+  puts
+  puts prompt
+  puts
+
+  while (true)
+    wait_for_user
+    puts
+    puts shake_ball + " " + random_answer
+    puts
+  end
+end
+erb :magic8ball
 
 end
