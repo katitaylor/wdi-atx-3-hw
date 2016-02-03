4302cb78868361b7cae4ee765be4ad32a47f2f8f
kati taylor <katherine.pelletier@gmail.com>
2016-02-03 10:19:29 -0600

    0802-updated

--- w08/d02/starter/API_twilio/routes/index.js
+++ w08/d02/starter/API_twilio/routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.post('/', function(req, res, next) {
-	// Your code goes here
-}
+	// Twilio Credentials
+
+var accountSid = 'AC8bcf44e7dd9ba19f2920ec3c2e3ed925';
+var authToken = '0d2fb52f52ba55c2a799a6397d52c347';
+var recipient = req.body.to;
+var textMessage = req.body.message;
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+  to: recipient,
+  from: "+12017442342",
+  body: "hello",
+}, function(err, message) {
+  console.log(message.sid);
+});
+});
 
 module.exports = router;
