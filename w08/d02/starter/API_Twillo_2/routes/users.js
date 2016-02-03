7561e77d8cbb269aad6119e8495c6d1220d4faa2
kati taylor <katherine.pelletier@gmail.com>
2016-02-03 09:21:55 -0600

    0802

--- w08/d02/starter/API_Twillo_2/routes/users.js
+++ w08/d02/starter/API_Twillo_2/routes/users.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
 });
-
-router.post('/', function(req, res, next) {
-	// Your code goes here
-}
 
 module.exports = router;
